d1320adafbb4bb2a9405721cbca74b9b57063fde
piyushkothari2020 <piyushkothari2020@gmail.com>
2023-04-17 17:55:19 +0530

    Created new file in Classes package

---
+++ src/Classes/SampleFile.java
@@ -0,0 +1,8 @@
+package Classes;
+
+public class SampleFile {
+
+    public static void main(String[] args){
+        System.out.println("Created new SampleClass file in Classes package!");
+    }
+}
