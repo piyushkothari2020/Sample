74340b49a3621ed3d47e305c8011210a576e8639
piyushkothari2020 <piyushkothari2020@gmail.com>
2023-04-20 15:21:31 +0530

    Added new line in SamplePage file of pages package

--- src/Pages/SamplePage.java
+++ src/Pages/SamplePage.java
@@ -5,5 +5,6 @@ public class SamplePage {
     public static void main(String[] args){
         System.out.println("Created new SamplePage file in Pages package!");
         System.out.println("Adding new line in this file");
+        System.out.println("Adding new line 3rd time!");
     }
 }
