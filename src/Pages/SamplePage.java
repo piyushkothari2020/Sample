f7c17354d8ece85ec4a6a8e2ab74a7897b32fcc8
piyushkothari2020 <117560405+piyushkothari2020@users.noreply.github.com>
2023-04-21 12:27:44 +0530

    Update SamplePage.java

--- src/Pages/SamplePage.java
+++ src/Pages/SamplePage.java
@@ -9,5 +9,6 @@ public class SamplePage {
         System.out.println("Adding new line 4th time in  github itself");
         System.out.println("Adding new line 5th time in  github itself");
         System.out.println("Adding new line 6th time in  github itself");
+        System.out.println("Adding new line 7th time in  github itself");
     }
 }
